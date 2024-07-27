8d2b9cfa046483edc8116f939d1642f51f6cf805
Steven Seidu <ayomikunseidu@gmail.com>
2024-07-27 14:11:08 +0100

    Update UserDetails.tsx

--- src/components/UserDetails/UserDetails.tsx
+++ src/components/UserDetails/UserDetails.tsx
@@ -30,7 +30,7 @@ const UserDetails: React.FC = () => {
             return;
         }
 
-        fetch(`https://steven-seidu-lendsqr-fe-test.vercel.app/api/users?index=${index}`)
+        fetch(`https://steven-seidu-lendsqr-fe-test.vercel.app/api/users?index=${userindex}`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
